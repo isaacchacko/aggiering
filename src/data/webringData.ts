17d69e6f4a87f3a04d6b902931fb9d71a8841a37
Isaac Chacko <41840952+isaacchacko@users.noreply.github.com>
2026-03-28 01:36:56 -0500

    Add webring member: Hitansh Mendiratta

--- src/data/webringData.ts
+++ src/data/webringData.ts
@@ -21,4 +21,9 @@ export const webringData: WebringMember[] = [
     website: "https://allenthomas.dev",
     year: "2028"
   },
+  {
+    name: "Hitansh Mendiratta",
+    website: "https://www.hitansh.us/",
+    year: "2028"
+  },
 ];
